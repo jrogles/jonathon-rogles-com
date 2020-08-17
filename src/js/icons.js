acbf8eda5142e4f50b70ecc7b6ea99ccda7c8928
Jonathon Rogles <jon.rogles@gmail.com>
2020-08-17 15:33:06 -0500

    bg image path fix

--- src/js/icons.js
+++ src/js/icons.js
@@ -2,22 +2,24 @@ import { library, dom } from "@fortawesome/fontawesome-svg-core";
 import { faEnvelope, faBrain, faPoll, faDrawPolygon, faCode, faEye, faInfinity, faGhost, faWaveSquare,faStoreAlt, faUserSecret } from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faTwitter, faLinkedinIn, faVimeo } from "@fortawesome/free-brands-svg-icons";
 
-library.add(
-    faEnvelope,
-    faBrain, 
-    faPoll, 
-    faDrawPolygon, 
-    faCode, 
-    faEye, 
-    faInfinity, 
-    faGhost, 
-    faWaveSquare,
-    faGithub, 
-    faStoreAlt, 
-    faUserSecret, 
-    faTwitter, 
-    faLinkedinIn, 
-    faVimeo 
-);
+export const icons = function() {
+    library.add(
+        faEnvelope,
+        faBrain, 
+        faPoll, 
+        faDrawPolygon, 
+        faCode, 
+        faEye, 
+        faInfinity, 
+        faGhost, 
+        faWaveSquare,
+        faGithub, 
+        faStoreAlt, 
+        faUserSecret, 
+        faTwitter, 
+        faLinkedinIn, 
+        faVimeo 
+    );
 
-dom.watch();
+    dom.watch();
+}();
